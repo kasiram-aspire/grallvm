0c466dd8438d530137f88da41a58dbb5f6cbb354
kasiram.palpandi <kasiram.palpandi@aspiresys.com>
2026-07-15 11:16:36 +0530

    fixed files

--- src/main/java/com/example/gralldemo/route/HelloRoute.java
+++ src/main/java/com/example/gralldemo/route/HelloRoute.java
@@ -9,7 +9,15 @@ public class HelloRoute extends RouteBuilder {
     @Override
     public void configure() {
 
-        from("platform-http:/hello?httpMethodRestrict=GET")
+        restConfiguration()
+                .component("platform-http");
+
+        rest("/hello")
+                .get()
+                .produces("text/plain")
+                .to("direct:hello");
+
+        from("direct:hello")
                 .setBody(constant("Hello from Camel"));
     }
 }
